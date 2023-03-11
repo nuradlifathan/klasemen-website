5fed8f9f83a5fe6cbd44f0c136f0c5c6da2122da
ADLI <nuradlifathan@gmail.com>
2023-03-11 23:31:24 +0700

    server routes

--- apps/server/routes/klasemen.routes.js
+++ apps/server/routes/klasemen.routes.js
@@ -2,6 +2,11 @@ const express = require("express")
 const klasemenControllers = require("../controllers/klasemen.controllers")
 const router = express.Router()
 
+// Create Club
 router.post("/create", klasemenControllers.createClubs)
+// Input Score
+router.post("/input-score", klasemenControllers.inputScore)
+// View Klasemen
+router.get("/klasemen", klasemenControllers.viewKlasemen)
 
 module.exports = router
