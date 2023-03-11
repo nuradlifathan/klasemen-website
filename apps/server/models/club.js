51c2248d9acc82f85d3279cc1bbf393a8d051ea8
ADLI <nuradlifathan@gmail.com>
2023-03-11 23:26:23 +0700

    refactor new models

---
+++ apps/server/models/club.js
@@ -0,0 +1,58 @@
+"use strict"
+const { Model } = require("sequelize")
+module.exports = (sequelize, DataTypes) => {
+  class Club extends Model {
+    static associate(models) {
+      Club.hasMany(models.Match, { onDelete: "CASCADE" })
+    }
+  }
+  Club.init(
+    {
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      main: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      menang: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      seri: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      kalah: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      goal_masuk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      goal_kemasukan: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+      point: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Club",
+      timestamps: false,
+    }
+  )
+  return Club
+}
