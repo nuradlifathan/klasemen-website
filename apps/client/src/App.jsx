509f6d84cf1d9b0acfe2122c577d831684d75872
RGS <nuradlifathan@gmail.com>
2023-03-13 02:32:53 +0700

    FEAT : fix layouting

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Flex, Text, Heading, Container, Stack } from "@chakra-ui/react"
+import { Flex, Container } from "@chakra-ui/react"
 import Sidebar from "./components/layout/Sidebar"
 import RoutePath from "./Routes"
 
@@ -7,26 +7,8 @@ function App() {
   return (
     <Flex w="100%">
       <Sidebar />
-
-      <Container maxW="3xl">
+      <Container maxW="fit-content">
         <RoutePath />
-
-        <Stack
-          textAlign="center"
-          spacing={{ base: 8, md: 14 }}
-          py={{ base: 20, md: 36 }}
-        >
-          <Heading
-            fontWeight={600}
-            fontSize={{ base: "2xl", sm: "4xl", md: "6xl" }}
-            lineHeight={"110%"}
-          >
-            Welcome to <br />
-            <Text as="span" color="teal.400">
-              Klasemen Website
-            </Text>
-          </Heading>
-        </Stack>
       </Container>
     </Flex>
   )
